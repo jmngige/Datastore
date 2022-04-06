9514a43d2154e9d660725bcf431ea4d6659ad07c
jmngige <jamesmngige@gmail.com>
2022-04-06 16:14:10 +0300

    Initial commit

--- app/src/main/java/com/starsolns/datastore/ui/HomeFragment.kt
+++ app/src/main/java/com/starsolns/datastore/ui/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.starsolns.datastore.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,7 +19,10 @@ class HomeFragment : Fragment() {
     ): View? {
         _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
 
-
+        binding.filterMoviesFab.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToBottomSheetFragment()
+            findNavController().navigate(action)
+        }
 
         return binding.root
     }
