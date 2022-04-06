642f4a2fc3a1d26df529b37f01878468c57c182e
jmngige <jamesmngige@gmail.com>
2022-04-06 14:08:19 +0300

    Initial commit

--- app/src/main/java/com/starsolns/datastore/MainActivity.kt
+++ app/src/main/java/com/starsolns/datastore/MainActivity.kt
@@ -2,10 +2,26 @@ package com.starsolns.datastore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.starsolns.datastore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHost.findNavController()
+
+        setupActionBarWithNavController(navController)
     }
 }
